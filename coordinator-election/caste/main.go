50d2ea9d91dc1846254a14f307dbacd54899968b
ccfortier <ccfortier@gmail.com>
2017-10-20 13:28:20 -0300

    listen

--- coordinator-election/caste/main.go
+++ coordinator-election/caste/main.go
@@ -3,15 +3,28 @@ package main
 import (
 	"fmt"
 	"os"
+	"github.com/dmichael/go-multicast/multicast"
+	"net"
+	"log"
+	"encoding/hex"
+	"time"
 )
+
+const (
+	defaultMulticastAddress = "224.0.0.100:9999"
+)
+
 
 func main() {
 	var input string
 	for {
+		time.Sleep(10)
 		fmt.Print("Enter text: ")
 		fmt.Scanln(&input)
 
 		switch input {
+		case "listen":
+			go listenMulticast()
 		case "bye":
 			fmt.Println("bye...")
 			os.Exit(0)
@@ -20,4 +33,14 @@ func main() {
 		}
 	}
 
+}
+
+func listenMulticast() {
+	fmt.Printf("Listening on %s\n", defaultMulticastAddress)
+	multicast.Listen(defaultMulticastAddress, msgHandler)
+}
+
+func msgHandler(src *net.UDPAddr, n int, b []byte) {
+	log.Println(n, "bytes read from", src)
+	log.Println(hex.Dump(b[:n]))
 }
